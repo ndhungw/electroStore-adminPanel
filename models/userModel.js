1004d28f4fbf6f1877a61b86e45c03bb4b39f18c
ndhungw <56079549+ndhungw@users.noreply.github.com>
2019-12-16 16:48:08 +0700

    chạy được đang sửa theo MVC, commit lại cho chắc

--- models/userModel.js
+++ models/userModel.js
@@ -1,8 +1,91 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
     name: { type: String, required: true},
 });
 
+let UsersModel = mongoose.model('User', UserSchema);
+
+UsersModel.paginatedResults = () => {
+    return async (req, res, next) => {
+        const page = parseInt(req.query.page);
+        const limit = parseInt(req.query.limit);
+
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+
+        const resultsss = {};
+
+        resultsss.current = {
+            page: page,
+            limit: limit
+        }
+
+        if (endIndex < await UsersModel.countDocuments().exec()) {
+            resultsss.next = {
+                page: page + 1,
+                limit: limit
+            };
+        }
+
+        if (startIndex > 0) {
+            resultsss.previous = {
+                page: page - 1,
+                limit: limit
+            };
+        }
+
+        try {
+            resultsss.results = await UsersModel.find().limit(limit).skip(startIndex).exec();
+            res.paginatedResults = resultsss;
+            next();
+        } catch (e) {
+            res.status(500).json({ message: e.message });
+        }
+    }
+}
+
+
+
+// function paginatedResults(model) {
+//     return async (req, res, next) => {
+//       const page = parseInt(req.query.page);
+//       const limit = parseInt(req.query.limit);
+      
+//       const startIndex = (page - 1) * limit;
+//       const endIndex = page * limit;
+      
+//       const resultsss = {};
+      
+//       resultsss.current = {
+//         page: page,
+//         limit: limit
+//       }
+      
+//       if (endIndex < await model.countDocuments().exec()) {
+//         resultsss.next = {
+//           page: page + 1,
+//           limit: limit
+//         };
+//       }
+      
+//       if (startIndex > 0) {
+//         resultsss.previous = {
+//           page: page - 1,
+//           limit: limit
+//         };
+//       }
+      
+//       try{
+//         resultsss.results = await model.find().limit(limit).skip(startIndex).exec();
+//         res.paginatedResults = resultsss;
+//         next();
+//       }catch (e) {
+//         res.status(500).json({message: e.message});
+//       }
+//     }
+//   }
+
 //                              name    schema      collection
-module.exports = mongoose.model('User',userSchema,'users');
+//module.exports = mongoose.model('User',userSchema,'users');
+module.exports = UsersModel;
