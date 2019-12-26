0d80de8d2435649e4bc8f50bd1789c04e6bcdac9
ndhungw <56079549+ndhungw@users.noreply.github.com>
2019-12-27 03:05:31 +0700

    add passport authentication

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production'){
+  require('dotenv').config();
+}
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -5,9 +9,15 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var expressHbs = require('express-handlebars');
 var bodyParser = require('body-parser');
-//var handlebarsHelper = require('./controllers/HandlebarsHelper');
+var passport = require('passport');
+var flash = require('express-flash');
+var session = require('express-session');
 
+var handlebarsHelper = require('./controllers/HandlebarsHelper');
 var database = require('./database');
+
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/usersRouter');
 
@@ -29,31 +39,29 @@ app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({   // to support URL-encoded bodies
   extended: true
 })); 
+app.use(flash());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//temp
+//var users = require('./models/userModel');
+//endtemp
+
+// var initializePassport = require('./controllers/passport-config');
+// initializePassport(
+//   passport, 
+//   email => users.find(user => user.email === email),
+//   id => users.find(user => user.id === id)
+// );
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-// //HandlebarsHelper
-// Handlebars.registerHelper('createPagination',
-// function (totalPage, currentPage){
-//   let arr = '';
-//   let i = 1;
-//   while(i <= totalPage){
-//     arr = arr.concat(`<li class="page-item ${i === currentPage ?'active' : ''}"><a class="page-link" href="/users/?page=${i}&limit=5">${i}</a></li>`);
-//     i++;
-//   }
-//   let result = `<nav aria-label="Page navigation">
-//   <ul class="pagination">
-//     <li class="page-item ${currentPage - 1 < 1 ? 'disabled' : ''}"><a class="page-link" href="/users?page=${currentPage - 1}&limit=5">Previous</a></li>
-//     ${arr}
-//     <li class="page-item ${currentPage + 1 > totalPage ? 'disabled' : ''}"><a class="page-link" href="/users?page=${currentPage + 1}&limit=5">Next</a></li>
-//   </ul>
-// </nav>`;
-//   console.log(result);
-//   return result;
-// }
-// );
-// //end HandlebarHelper
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+
+
+
+
+// test login for admin
+var passport = require('passport')//--------------------
+const bcrypt = require('bcrypt');
+const users = [];
+
+var initializePassport = require('../controllers/passport-config');
+initializePassport(
+  passport, 
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('pages/index', { title: 'Express' });
@@ -10,12 +26,6 @@ router.get('/', function(req, res, next) {
 router.get('/index.html', function(req, res, next) {
   res.render('pages/index', { title: 'Express' });
 });
-
-
-// test login for admin
-const users = [];
-const bcrypt = require('bcrypt');
-
 router.get('/login', (req, res) => {
   res.render('pages/login');
 });
@@ -24,21 +34,25 @@ router.get('/register', (req, res) => {
   res.render('pages/register');
 });
 
-router.post('/login', (req, res) => {
-
-});
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: 'login',
+  failureFlash: true
+}));
 
 router.post('/register', async (req, res) => {
   try {
-    const hashedPassword = await bcrypt.hash(req.body.userPassword, 10);
+    //const hashedPassword = await bcrypt.hash(req.body.userPassword, 10);
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
     users.push({
-      //id: Date.now().toString()
-      name: req.body.userFullName,
-      email: req.body.userEmail,
+      id: Date.now().toString(),
+      name: req.body.name,//userFullName
+      email: req.body.email,//userEmail
       password: hashedPassword
     })
     res.redirect('/login')
-  } catch {
+  } catch (e) {
+    console.log('error : ' + e);
     res.redirect('/register')
   }
   console.log(users);
